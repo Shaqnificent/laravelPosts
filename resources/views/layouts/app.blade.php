437c51c482cfc9a1c94b03993669738681303bd9
Shaqnificent <shaqnificent@gmail.com>
2022-06-14 14:55:05 -0500

    fixed tailwind configs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Post</title>
+    <title>Posty</title>
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<body class="bg-black">
+<body class="bg-gray-400">
+    <nav class="p-6 bg-gray-200 flex justify-between">
+        <ul class="flex items-center">
+            <li><a href="">Home</a></li>
+            <li><a href="">Dashboard</a></li>
+            <li><a href="">Posts</a></li>
+        </ul>
+    </nav>
     @yield('content')
 </body>
 </html>
