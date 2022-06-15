856c378329c6a3059e9e59f15b08fb25b226bd14
Shaqnificent <shaqnificent@gmail.com>
2022-06-14 20:01:47 -0500

    added dashboard view

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex justify-center my-1">
+        <div class="w-8/12 bg-white p-6 rounded-lg">
+            Dashboard
+        </div>
+
+    </div>
+@endsection
