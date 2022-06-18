b9cf75892581a9a77038456fb75aa56b90d59a0c
Shaqnificent <shaqnificent@gmail.com>
2022-06-17 20:42:27 -0500

    added posting functionality

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,4 +10,22 @@ class PostController extends Controller
     {
         return view('posts.index');
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'body' => 'required'
+        ]);
+
+        // Post::create([
+        //     'user_id' => auth()->id,
+        //     'body' => $request->body
+        // ]);
+
+        $request->user()->posts()->create([
+            'body' => $request->body
+        ]);
+
+        return back();
+    }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,6 +4,7 @@
     <div class="flex justify-center my-1">
         <div class="w-8/12 bg-white p-6 rounded-lg">
             <form action="{{route('posts')}}" method="post">
+                @csrf
                 <div class="mb-4">
                     <label for="body" class="sr-only">Body</label>
                     <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror" placeholder="PostSomething!"></textarea>
